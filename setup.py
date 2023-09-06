7eeee6753b45538a316b3aae807e782ec879d7e9
fkolby <frederikkoelby@gmail.com>
2023-09-06 16:20:34 +0200

    Created project folders and structure.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Estimating depths from a single image, with uncertainty.",
+    author="Frederik Kølby Christensen",
+    license="",
+)
