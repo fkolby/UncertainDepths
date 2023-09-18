d04de2278c270a282692cb313efd1d72189951b6
fkolby <frederikkoelby@gmail.com>
2023-09-18 16:07:45 +0200

    Needs fixing of contracting network, of linear activation function, and of datamodule

--- src/models/modelImplementations/baseUNet.py
+++ src/models/modelImplementations/baseUNet.py
@@ -53,15 +53,15 @@ class UpSample(nn.Sequential):
 
 class CropAndConcatenateOp(nn.Module):
     def forward(self, x: torch.Tensor, contracting_x: torch.Tensor):
-        contracting_x = torchvision.transforms.functional.center_crop(
-            contracting_x, (x.shape[2], x.shape[3])
-        )
+        contracting_x = torchvision.transforms.CenterCrop(
+             (x.shape[2], x.shape[3])
+        )(contracting_x)
         x = torch.cat([x, contracting_x], dim=1)
         return x
 
 
 class BaseUNet(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int):
+    def __init__(self, in_channels: int, out_channels: int, in_debug:bool = False):
         super().__init__()
         self.down_conv = nn.ModuleList(
             [DoubleConv(i, o) for (i, o) in [(in_channels, 64), (64, 128), (128, 256), (256, 512)]]
@@ -77,43 +77,44 @@ class BaseUNet(nn.Module):
         )
         self.concat = nn.ModuleList([CropAndConcatenateOp() for _ in range(4)])
         self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+        self.in_debug = in_debug
 
     def forward(self, x: torch.Tensor):
         pass_through = []
-        debugxshape(x)
+        debugxshape(x, in_debug =True )
+        print(x)
 
         for i in range(len(self.down_conv)):  # contract
             x = self.down_conv[i](x)
-            debugxshape(x)
+            debugxshape(x, in_debug = self.in_debug)
             pass_through.append(x)
-            debugxshape(x)
+            debugxshape(x, in_debug = self.in_debug)
             x = self.down_sample[i](x)
-            debugxshape(x)
+            debugxshape(x, in_debug = self.in_debug)
 
         x = self.middle_conv(x)
-        debugxshape(x)
+        debugxshape(x,in_debug=self.in_debug)
 
         for i in range(len(self.up_conv)):  # upsample and scale
             x = self.up_sample[i](x)
-            debugxshape(x)
+            debugxshape(x,in_debug=self.in_debug)
             x = self.concat[i](x, contracting_x=pass_through.pop())
-            debugxshape(x)
+            debugxshape(x,in_debug=self.in_debug)
             x = self.up_conv[i](x)
-            debugxshape(x)
+            debugxshape(x,in_debug=self.in_debug)
 
         x = self.final_conv(x)
-        debugxshape(x)
+        debugxshape(x,in_debug=self.in_debug)
 
         return x
 
 
 if __name__ == "__main__":
-    save_config()
-    x = torch.rand(1, 3, 572, 572)
-    x = BaseUNet(3, 2)(x)
-    debugxshape(x)
+    x = torch.rand(8, 3, 572, 572)
+    x = BaseUNet(3, 1, in_debug = True)(x)
+    debugxshape(x,in_debug=True)
 
-    a_based_u_net = BaseUNet(3, 2)
+    a_based_u_net = BaseUNet(3, 1, in_debug = True)
     summary(a_based_u_net, (1, 3, 572, 572))
 
     from torchview import draw_graph
@@ -123,4 +124,3 @@ if __name__ == "__main__":
 
     graph_of_model.visual_graph.view()
 
-    shape_and_print_tensor(x)
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -5,9 +5,8 @@ from src.models.modelImplementations.baseUNet import BaseUNet
 from src.data.datamodules import KITTIDataModule
 import pytorch_lightning as pl
 from torch import optim, nn
-
-
-class KITTI_depth(pl.LightningModule):
+import pdb
+class KITTI_depth_lightning_module(pl.LightningModule):
     def __init__(self, model, loss_function, learning_rate):
         super().__init__()
         self.model = model
@@ -17,9 +16,26 @@ class KITTI_depth(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x, y = batch
         preds = self.model(x)
+        print(f"shape of x: {x.shape}, yshape: {y.shape}, shape of preds: {preds.shape}")
         loss = self.loss_function(preds, y)
         self.log("train_loss", loss)
         return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        preds = self.model(x)
+        loss = self.loss_function(preds, y)
+        self.log("validation_loss", loss)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        preds = self.model(x)
+        loss = self.loss_function(preds, y)
+        self.log("TEST loss", loss)
+        return loss
+
+
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=self.lr)
@@ -31,14 +47,22 @@ def main(cfg: DictConfig):
     log = logging.getLogger(__name__)
     log.info(OmegaConf.to_yaml(cfg))
     loss_function = nn.functional.mse_loss
-    KITTI_depth_Module = KITTI_depth(
-        model=BaseUNet(in_channels=4, out_channels=2),
+    model = KITTI_depth_lightning_module(
+        model=BaseUNet(in_channels=3, out_channels=1),
         loss_function=loss_function,
         learning_rate=cfg.hyperparameters.learning_rate,
     )
-    trainer = pl.Trainer()
+    trainer = pl.Trainer(num_sanity_val_steps=0)
+
     datamodule = KITTIDataModule(data_dir=cfg.data_dir, batch_size=cfg.hyperparameters.batch_size)
-    trainer.fit(model=KITTI_depth_Module, datamodule=datamodule)
+    debugdatamodule = KITTIDataModule(data_dir=cfg.data_dir, batch_size=cfg.hyperparameters.batch_size)
+    debugdatamodule.setup("fit")
+    print(type(debugdatamodule.train_dataloader()))
+    print(iter(debugdatamodule.train_dataloader()).__next__())
+    print("is dataloader iterable?", hasattr(debugdatamodule.train_dataloader(),"__iter__"))
+    print("now comes error")
+    datamodule.setup(stage="fit")
+    trainer.fit(model=model, train_dataloaders= datamodule.train_dataloader())
 
 
 if __name__ == "__main__":
