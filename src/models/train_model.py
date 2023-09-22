d5864d2d469c4a48f1c93602e0ea5468952d36bc
fkolby <frederikkoelby@gmail.com>
2023-09-22 15:06:47 +0200

    Visualization now works.

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,4 +1,6 @@
 import logging
+import PIL
+import seaborn as sns
 import torch
 import wandb
 from torchvision import transforms
@@ -11,51 +13,38 @@ import pytorch_lightning as pl
 from torch import optim, nn
 from pytorch_lightning import callbacks
 import pdb
+from src.utility.viz_utils import log_images
 
 
 class KITTI_depth_lightning_module(pl.LightningModule):
-    def __init__(self, model, loss_function, learning_rate):
+    def __init__(self, model, loss_function, learning_rate, min_depth, max_depth):
         super().__init__()
         self.model = model
         self.loss_function = loss_function
         self.lr = learning_rate
         self.tstep = 0
+        self.min_depth = min_depth
+        self.max_depth = max_depth
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        preds = self.model(x)
+        pred = self.model(x)
+        print(f"x: {x.shape} y: {y.shape}, pred: {pred.shape}")
+        #img=torch.squeeze(x[0, :, :, :].detach(), dim=0)
+        #depth=torch.squeeze(y[0, :, :, :].detach(), dim=0)
+        #pred=torch.squeeze(pred[0, :, :, :].detach(), dim=0)
+        #print(img.shape,depth.shape,pred.shape)
         if self.tstep % 10 == 0:
-            imgA = transforms.ToPILImage()(x[0, :, :, :].squeeze(dim=0))
-            print(type(imgA))
-            wandb.log(
-                {
-                    "images": [
-                        wandb.Image(imgA, "RGB", caption=f"Image {self.tstep} (input)"),
-                        wandb.Image(
-                            transforms.ToPILImage()(y[0, :, :, :].squeeze(dim=0)),
-                            "RGB",
-                            caption=f"Image {self.tstep} (label)",
-                        ),
-                        wandb.Image(
-                            transforms.ToPILImage()(preds[0, :, :, :].squeeze(dim=0)),
-                            "RGB",
-                            caption=f"Image {self.tstep} (preds)",
-                        ),
-                        wandb.Image(
-                            transforms.ToPILImage()((y-preds)[0, :, :, :].squeeze(dim=0)),
-                            "RGB",
-                            caption=f"Image {self.tstep} (y-preds)",
-                        ),
-                        wandb.Image(
-                            transforms.ToPILImage()(torch.abs(y-preds)[0, :, :, :].squeeze(dim=0)),
-                            "RGB",
-                            caption=f"Image {self.tstep} abs(y-preds)",
-                        ),
-                    ]
-                }
+            log_images(
+                img=torch.squeeze(x[0, :, :, :].detach(), dim=0),
+                depth=torch.squeeze(y[0, :, :, :].detach(), dim=0),
+                pred=torch.squeeze(pred[0, :, :, :].detach(), dim=0),
+                vmin=self.min_depth,
+                vmax=self.max_depth,
+                step=self.tstep,
             )
         self.tstep += 1
-        loss = self.loss_function(preds, y)
+        loss = self.loss_function(pred, y)
 
         self.log("train_loss", loss)
         return loss
@@ -89,25 +78,40 @@ def main(cfg: DictConfig):
         model=BaseUNet(in_channels=3, out_channels=1),
         loss_function=nn.functional.mse_loss,
         learning_rate=cfg.hyperparameters.learning_rate,
+        min_depth=cfg.dataset_params.min_depth,
+        max_depth=cfg.dataset_params.max_depth,
     )
     trainer = pl.Trainer(max_epochs=1, logger=logger, limit_train_batches=0.01)
-    transform = transforms.Compose([transforms.PILToTensor(),
-                                    transforms.Lambda(lambda x: x/255),
-                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),#normalize using imagenet values, as I have yet not calced it for KITTI.
-                                    transforms.Pad(padding=(3, 4, 3, 5))])
-    target_transform = transforms.Compose([transforms.PILToTensor(),
-                                    transforms.Lambda(lambda x: x/255),
-                                    transforms.Pad(padding=(3, 4, 3, 5))])
+    transform = transforms.Compose(
+        [
+            transforms.PILToTensor(),
+            transforms.Lambda(lambda x: x / 255),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            ),  # normalize using imagenet values, as I have yet not calced it for KITTI.
+            transforms.Pad(padding=(3, 4, 3, 5)),
+        ]
+    )
+    target_transform = transforms.Compose(
+        [
+            transforms.PILToTensor(),
+            transforms.Lambda(lambda x: x / 255),
+            transforms.Pad(padding=(3, 4, 3, 5)),
+        ]
+    )
     datamodule = KITTIDataModule(
-        data_dir=cfg.data_fetching_params.data_dir,
+        data_dir=cfg.dataset_params.data_dir,
         batch_size=cfg.hyperparameters.batch_size,
         transform=transform,
         target_transform=target_transform,
-        num_workers=cfg.data_fetching_params.num_workers,
+        num_workers=cfg.dataset_params.num_workers,
     )
-    
+
     datamodule.setup(stage="fit")
-    trainer.fit(model=model, train_dataloaders=datamodule.train_dataloader(),val_dataloaders= datamodule.val_dataloader())
+    trainer.fit(
+        model=model,
+        train_dataloaders=datamodule.train_dataloader(),
+    )  # val_dataloaders= datamodule.val_dataloader())
 
 
 if __name__ == "__main__":
---
+++ src/utility/viz_utils.py
@@ -0,0 +1,56 @@
+import matplotlib.cm
+import pdb
+import wandb
+import numpy as np
+import torch
+from torchvision import transforms
+
+
+## function found from ADABINS (https://github.com/shariqfarooq123/AdaBins/blob/main/utils.pdepth)
+def colorize(value, vmin=10, vmax=1000, cmap="plasma"):
+    # normalize
+    vmin = value.min() if vmin is None else vmin
+    vmax = value.max() if vmax is None else vmax
+    if vmin != vmax:
+        value = (value - vmin) / (vmax - vmin)  # vmin..vmax
+    else:
+        # Avoid 0-division
+        value = value * 0.0
+    # squeeze last dim if it exists
+    # value = value.squeeze(axis=0)
+
+    cmapper = matplotlib.cm.get_cmap(cmap)
+    value = cmapper(value, bytes=True)  # (nxmx4)
+
+    img = value[:, :, :3]
+    #     return img.transpose((2, 0, 1))
+    return img
+
+
+def log_images(img, depth, pred, vmin, vmax, step):
+    depthdiff = colorize((depth - pred), vmin=vmin, vmax=vmax)
+    absdepthdiff = colorize(torch.abs(depth - pred), vmin=vmin, vmax=vmax)
+    depth = colorize(depth, vmin=vmin, vmax=vmax)
+    pred = colorize(pred, vmin=vmin, vmax=vmax)
+    wandb.log(
+        {
+            "images": [
+                wandb.Image(transforms.ToPILImage()(img), "RGB", caption=f"Image {step} (input)"),
+                wandb.Image(depth, "RGB", caption=f"Image {step} (label)"),
+                wandb.Image(
+                    pred,
+                    "RGB",
+                    caption=f"Image {step} (pred)",
+                ),
+                wandb.Image(depthdiff,
+                    "RGB",
+                    caption=f"Image {step} (depth-pred)",
+                ),
+                wandb.Image(absdepthdiff,
+                    "RGB",
+                    caption=f"Image {step} abs(depth-pred)",
+                ),
+            ]
+        },
+        step=step,
+    )
