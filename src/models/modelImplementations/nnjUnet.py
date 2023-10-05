2fe388dc6d4d0465c31154efb22473751a7a3130
jbv415 <jbv415@hendrixgate03fl.unicph.domain>
2023-10-05 13:51:02 +0200

    Added Flatten and SkipConnection to nnjUnet

--- src/models/modelImplementations/nnjUnet.py
+++ src/models/modelImplementations/nnjUnet.py
@@ -1,13 +1,65 @@
-from stochman import nnj
+import nnj
 import torch
+import src.utility.train_utils as utils
+from src.utility.debug_utils import debugxshape, shape_and_print_tensor
+from typing import Union, Literal
 
 
-class UNet_stochman_64(torch.nn.Module):
+import torch
+import torchvision
+from torch import nn, Tensor
+import time
+from torchinfo import summary
+import pdb
+
+class SkipConnect(nnj.AbstractJacobian, nn.Module):
+    def __init__(self, *args, add_hooks: bool=False):
+        super().__init__()
+        self._F = nnj.Sequential(*args, add_hooks=add_hooks)
+        self._n_params = 0
+
+    def forward(self, x: Tensor):
+        return torch.cat([x, self._F(x)], dim=1)
+    
+    @torch.no_grad()
+    def jacobian(self, x: torch.Tensor, val: Union[None, torch.Tensor], wrt : Literal["input","weight"] = "input"):
+        if val == None:
+            return self.forward(x)
+
+        if wrt == "weight":
+            #Non parametric layer, so does not have any wrt weight
+            return None
+        else:
+            raise NotImplementedError
+
+
+class Flatten(nnj.AbstractJacobian, nn.Module):
+    #NOT SURE ABOUT INIT: NOT INCLUDED IN MARCO?!
     def __init__(self):
+        super().__init__()
+        self._n_params=0
+
+    def forward(self, x: Tensor):
+        return x.flatten(start_dim = 1, end_dim = -1)
+    
+    @torch.no_grad()
+    def jacobian(self, x: torch.Tensor, val: Union[None, torch.Tensor], wrt : Literal["input","weight"] = "input"):
+        if val == None:
+            return self.forward(x)
+
+        if wrt == "weight":
+            #Non parametric layer, so does not have any wrt weight
+            return None
+        else:
+            raise NotImplementedError
+
+pdb.set_trace()
+class UNet_stochman_64(torch.nn.Module):
+    def __init__(self, in_chan, out_chan):
         super().__init__()
 
         self.stochastic_net = nnj.Sequential(
-            nnj.Conv2d(3, 8, 3, stride=1, padding=1),
+            nnj.Conv2d(in_chan, 8, 3, stride=1, padding=1),
             nnj.Tanh(),
             nnj.Conv2d(8, 8, 3, stride=1, padding=1),
             nnj.Tanh(),
@@ -79,9 +131,23 @@ class UNet_stochman_64(torch.nn.Module):
             nnj.Tanh(),
             nnj.Conv2d(8, 8, 3, stride=1, padding=1),
             nnj.Tanh(),
-            nnj.Conv2d(8, 1, 1, stride=1, padding=0),
+            nnj.Conv2d(8, out_chan, 1, stride=1, padding=0),
             add_hooks=True,
         )
 
     def forward(self, x):
         return self.stochastic_net(x)
+        
+
+
+if __name__ == "__main__":
+        
+    a_based_u_net = UNet_stochman_64(in_chan = 3, out_chan = 1)
+    summary(a_based_u_net, (1, 3, 704, 352))
+
+    from torchview import draw_graph
+
+    graph_of_model = draw_graph(a_based_u_net, input_size=(1, 3, 1024, 608))
+    graph_of_model.visual_graph
+
+    graph_of_model.visual_graph.view()
