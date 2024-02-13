94f13ab8b61315eeae49754f84814d586524a30f
fkolby <frederikkoelby@gmail.com>
2024-02-13 10:48:31 +0100

    Updated workers and configs to avoid cgroup OOM

--- src/no_debug_run.sh
+++ src/no_debug_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --output=slurm-folder/slurm_%j.out
-#SBATCH --gres=gpu:a40:1 --time=6-00:00:00  --cpus-per-task=8 --mem-per-cpu=8192
+#SBATCH --gres=gpu:a100:1 --time=2-00:00:00  --cpus-per-task=8 --mem-per-cpu=8G
 #python models/train_model.py in_debug=False save_images=True models=Posthoc_Laplace neural_net_param_multiplication_factor=32 hyperparameters.learning_rate=3e-4 transforms.rand_aug=False trainer_args.max_epochs=150 
 #python models/train_model.py models=Dropout in_debug=False save_images=True neural_net_param_multiplication_factor=32 hyperparameters.learning_rate=3e-4 transforms.rand_aug=False trainer_args.max_epochs=50 #models.hessian_memory_factor=0.9999 models.hessian_scale=1e+4 models.update_hessian_probabilistically=True models.update_hessian_every=10 models.sample_last_n_epochs=50 models.dont_sample_parameters_during_training=False models.use_exp_average_instead=False
 #python models/train_model.py models=Ensemble in_debug=False save_images=True neural_net_param_multiplication_factor=32 hyperparameters.learning_rate=3e-4 transforms.rand_aug=False trainer_args.max_epochs=100 #models.hessian_memory_factor=0.9999 models.hessian_scale=1e+4 models.update_hessian_probabilistically=True models.update_hessian_every=10 models.sample_last_n_epochs=50 models.dont_sample_parameters_during_training=False models.use_exp_average_instead=False
 
-python models/train_model.py hyperparameters.batch_size=8 models.hessian_initial_multiplication_factor=1e+4 models=Online_Laplace in_debug=False save_images=True neural_net_param_multiplication_factor=32 hyperparameters.learning_rate=3e-4 transforms.rand_aug=False trainer_args.max_epochs=50 models.hessian_memory_factor=0.99999 models.hessian_scale=1 models.update_hessian_probabilistically=True models.update_hessian_every=10  models.dont_sample_parameters_during_training=False models.use_exp_average_instead=False models.sample_last_n_epochs=1000
+python models/train_model.py hyperparameters.batch_size=16 models.hessian_initial_multiplication_factor=1e+4 models=Online_Laplace in_debug=False save_images=True neural_net_param_multiplication_factor=32 hyperparameters.learning_rate=3e-4 transforms.rand_aug=False trainer_args.max_epochs=100 models.hessian_memory_factor=0.9999 models.hessian_scale=1e+4 models.update_hessian_probabilistically=True models.update_hessian_every=10  models.dont_sample_parameters_during_training=False models.use_exp_average_instead=False models.sample_last_n_epochs=1000
 
 
 
